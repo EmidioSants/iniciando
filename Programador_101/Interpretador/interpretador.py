80e05f8a2e10e5147a78234f517a06f2054648bd
EmidioSants <clebersonemidiosantos@gmail.com>
2023-07-12 19:25:16 -0300

    Primeiro arquivo do projeto 101

---
+++ Programador_101/Interpretador/interpretador.py
@@ -0,0 +1,9 @@
+titulo = "Interpretador"
+nome = "Emidio"
+sobrenome = "Santos"
+idade = 2023 - 1992
+
+print(f'{titulo:*^50}\n'
+    f'Nome: {nome} \n'
+    f'Sobrenome: {sobrenome} \n'
+    f'Idade: {idade}')
