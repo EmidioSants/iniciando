285f54b2cc7e34168156ee59bf02174c54183c14
EmidioSants <clebersonemidiosantos@gmail.com>
2023-07-12 20:23:29 -0300

    Comentário sobre a função print

---
+++ Programador_101/funcao_print/print.py
@@ -0,0 +1,6 @@
+#A função print() é uma das funções mais importantes e usadas na linguagem Python.
+# Sua função é, basicamente, exibir mensagens na tela ou enviá-las para outro dispositivo,
+# como imprimir dentro de arquivos de texto
+
+
+print(12, 13, sep='-')
